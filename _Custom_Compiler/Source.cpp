9954ee9094103fa9c699d98898cd858a6442e00c
AbdulMoqeet87 <abdulmoqeet87@gmail.com>
2025-11-09 00:48:24 +0500

    Modidfied main

--- _Custom_Compiler/Source.cpp
+++ _Custom_Compiler/Source.cpp
@@ -1,27 +1,25 @@
 #include<iostream>
-//#include "Without_regex_Lexer.h"
+#include "Without_regex_Lexer.h"
 #include"with_regex_Lexer.h"
-#include"Parser.h"
-//#include"Parser2.h"
+//#include"Parser.h"
+#include"Parser2.h"
 
 using namespace std;
 
 
-int main() {
-    // 1. run the lexer
-    Lexer_regex lexer;
-	lexer.GenerateTokens("text.txt");
-
-    cout << "tokens:\n";
-    lexer.PrintTokens();
-
-    // 2. run the parser
-    vector<token> tokens = lexer.getTokens();
-	Parser parser(tokens);
-	parser.parseProgram()->print();
-
-    return 0;
-}
+//int main12() {
+//    // 1. Run the lexer
+//    Lexer_regex lexer;
+//    lexer.GenerateTokens("Text.txt");
+//
+//    cout << "Tokens:\n";
+//    lexer.PrintTokens();
+//
+//    // 2. Run the parser
+//    vector<token> tokens = lexer.getTokens();
+//    Parser parser(tokens);
+//    parser.parseProgram();
+//}
 
 //int main()
 //{
